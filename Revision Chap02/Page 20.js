9562d403b9fd8c94eddd4f022ea4d8f481829f3a
Nsika Nxumalo <nxumalonsika83@gmail.com>
2026-09-09 10:42:56 +0200

    Add files via upload

---
+++ Revision Chap02/Page 20.js
@@ -0,0 +1,15 @@
+var secondsInAMinute = 60;
+var minutesInAnHour = 60;
+var secondsInAnHour = secondsInAMinute * minutesInAnHour;
+secondsInAnHour;
+3600
+
+var hoursInADay = 24;
+var secondsInADay = secondsInAnHour * hoursInADay;
+u secondsInADay;
+86400
+
+var daysInAYear = 365;
+var secondsInAYear = secondsInADay * daysInAYear;
+secondsInAYear;
+31536000
